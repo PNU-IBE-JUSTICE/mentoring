a991946bf4efec6af2a2e0d353a00cb913933ba5
yeseul-kim01 <yesul0718@naver.com>
2024-08-17 14:32:15 +0900

    0817세션수정완료_세션아웃가능

--- src/main/java/pnu/ibe/justice/mentoring/config/auth/SecurityConfig.java
+++ src/main/java/pnu/ibe/justice/mentoring/config/auth/SecurityConfig.java
@@ -33,10 +33,10 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests((authorizeRequest) -> authorizeRequest
                         //.requestMatchers("/admin/**").hasRole(Role.ADMIN.name())
-                        //.requestMatchers("/admin/**").hasRole(Role.ADMIN.name())
+                        .requestMatchers("/admin/**").hasRole(Role.ADMIN.name())
                         .requestMatchers("/posts/new", "/comments/save").hasRole(Role.GUEST.name())
                         .requestMatchers("/","/h2-console/**", "/login/*",
-                                "/logout/*", "/static/**","/static/favicon.ico","/static/lib/**", "/static/css/**","/static/js/**","/static/images/**","/static/scss/**").permitAll()
+                                "/logout/*","/favicon.ico","/lib/**", "/css/**","/js/**","/images/**","/scss/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .logout( // 로그아웃 성공 시 / 주소로 이동
