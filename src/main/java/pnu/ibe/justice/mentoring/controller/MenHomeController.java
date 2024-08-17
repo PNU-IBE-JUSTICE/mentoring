f020ee60de28b8c6fdf3aec9b61b36f7bf00e3de
reetaegkim <reetaeg@naver.com>
2024-08-17 22:19:28 +0900

    kids layout 적용. notice 적용즁.

--- src/main/java/pnu/ibe/justice/mentoring/controller/MenHomeController.java
+++ src/main/java/pnu/ibe/justice/mentoring/controller/MenHomeController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pnu.ibe.justice.mentoring.config.auth.LoginUser;
 import pnu.ibe.justice.mentoring.config.auth.SessionUser;
@@ -20,12 +21,14 @@ public class MenHomeController {
 
     private final HttpSession httpSession;
 
+    @ModelAttribute("user")
+    public SessionUser getSettings(@LoginUser SessionUser user) {
+        return user;
+    }
 
     @GetMapping("")
-    public String index(Model model,  @LoginUser SessionUser user) {
-        if (user != null) {
-            model.addAttribute("userName", user.getName());
-        }
+    public String index(Model model) {
+
         return "/index";
     }
 
